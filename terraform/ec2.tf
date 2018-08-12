5a27e2b5f129f4f4ea3c3a52e15b4e02d9b3a0cd
Teppy Oketunji <toketunji@gmail.com>
2018-08-12 19:00:15 +0100

    updated ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "jenkins_masters" {
   instance_type                 =       "t2.micro"
   subnet_id                     =       "${aws_subnet.jenkins_public.id}"
   key_name                      =       "${var.key_name}"
-  user_data                     =       "${data.template_file.jenkins_master.rendered}"
+#  user_data                     =       "${data.template_file.jenkins_master.rendered}"
   associate_public_ip_address   =       true
   security_groups               =       ["${aws_security_group.jenkins.id}"]
   count                         =       "1"
